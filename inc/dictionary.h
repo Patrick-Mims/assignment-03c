b8b249abe49b28ced667573442b9992df4dd7a94
Patrick Mims <patrickkevinmims@gmail.com>
2021-07-02 18:49:01 -0700

    Working latest

--- inc/dictionary.h
+++ inc/dictionary.h
@@ -10,11 +10,19 @@
 
 using namespace std;
 
+void vector_search_definitions(vector<string> &d)
+{
+    for (vector<string>::iterator it = d.begin(); it != d.end(); ++it)
+    {
+        cout << *it << endl;
+    }
+}
+
 void vector_search_grammar(vector<string> &g)
 {
-    for (vector<string>::iterator t = g.begin(); t != g.end(); ++t)
+    for (vector<string>::iterator it = g.begin(); it != g.end(); ++it)
     {
-        cout << "Searching Grammar Vector => " << *t << endl;
+        cout << "Searching Grammar Vector => " << *it << endl;
     }
 }
 
@@ -59,10 +67,12 @@ void loadData()
 {
     vector<string> vec;
     vector<string> vGrammar;
+    vector<string> vDefinition;
 
     char copy[256];
 
     string definition;
+    string defSet;
     string grammar;
     string separator;
     string inDataFile = "Data.CS.SFSU.txt";
@@ -74,40 +84,54 @@ void loadData()
     cout << "! Loading Data..." << endl;
 
     ifstream in_gram(inDataFile.c_str());
-    ofstream out_gram(outDataFile.c_str());
+    if (in_gram.fail())
+    {
+        cout << "Couldn't open the file!" << endl;
+    }
 
     cout << "! Loading Completed..." << endl;
 
-    while (getline(in_gram, grammar, '|'))
-    {
-        in_gram >> grammar >> separator;
-        //cout << "Grammar: " << grammar << endl;
-        vGrammar.push_back(grammar);
-    }
+    string item;
 
     cout << "! Closing Data File..." << endl;
 
-    out_gram.close();
     in_gram.close();
 
     ifstream in(inDataFile.c_str());
 
+    char str[255];
+
+    string tp;
+    while (getline(in, tp, '|'))
+    {
+        vDefinition.push_back(tp);
+    }
+
+    /*
     while (in >> lineOne >> lineTwo)
     {
+        cout << "this is lineTwo: " << lineTwo << endl;
         string parsedData = parseFile(in);
         vec.push_back(parsedData);
         //cout << parsedData << endl;
     }
+    */
 
     in.close();
 
     cout << "vec: size: " << vec.size() << endl;
     cout << "gram: size: " << vGrammar.size() << endl;
 
-    vector_search_grammar(vGrammar);
+    //vector_search_definitions(vDefinition);
+
+    //vector_search_grammar(vGrammar);
 }
 
 /*
+void loadDefinitions()
+{
+}
+
 void loadData()
 {
     vector<string> vec;
